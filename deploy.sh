7dd708b662dcd77703bfcaeb9c9b5d3a9082770d
Danylo Hermanovych <elluyema@gmail.com>
2024-05-05 16:23:15 +0300

    fix deploy yml

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 docker pull hdanylo/auction-backend
-docker run -p 3000:3000 hdanylo/auction-backend
+docker run -p 3000:3000 -e DATABASE_URL={{DATABASE_URL}} -e TOKEN_SECRET={{TOKEN_SECRET}} -e TOKEN_EXPIRES_IN={{TOKEN_EXPIRES_IN}} hdanylo/auction-backend
