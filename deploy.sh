299fb190eaf73ceb824ba80d719c687952ff47e6
Danylo Hermanovych <elluyema@gmail.com>
2024-06-02 11:04:30 +0300

    fix problems

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@ docker stop backend
 docker rm backend
 docker image rm hdanylo/auction-backend
 docker pull hdanylo/auction-backend
+
 docker run -p 3000:3000 --name backend -d --env-file ./env hdanylo/auction-backend
